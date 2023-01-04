76a533a12748d90c58c86381d1af57a9845e4c8a
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2023-01-04 00:24:49 -0300

    aula 1 e 2 - criando banco de dados e tabelas

---
+++ MySQL.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE Db_World;
+
+USE Db_World;
+
+CREATE TABLE tb_country
+(nome VARCHAR (100),
+continente VARCHAR (20));
+
+CREATE TABLE tb_lenguage 
+(idioma VARCHAR(20));
+
+
+CREATE TABLE tb_city
+(nomeCidades VARCHAR (100),
+habitantes BIGINT);
+
